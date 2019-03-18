7fc4d7185396b6981cdf4e0ff10ee5f90672b8f3
godfreyJo <godfreyoyugi@gmail.com>
2019-03-18 16:51:42 +0300

    adding spikle

--- server.py
+++ server.py
@@ -1,21 +1,27 @@
 import socket
+import time
+import pickle
+
+
 
 HEADERSIZE = 10
-
+ 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind((socket.gethostname(), 1234))
+s.bind((socket.gethostname(), 1236))
 s.listen(5)
 
 while True:
 	clientsocket, address = s.accept()
 	print(f"Connection form {address} has been established!")
 
-	msg = "Welcome to the sever!"
-	msg = f'{len(msg):<{HEADERSIZE}}' + msg
+	d = {1: "hey", 2:"There"}
+	msg = pickle.dumps(d)
+
+
+	msg =bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8")  + msg
 	
 
 
-	clientsocket.send(bytes("Welcome to the server!","utf-8"))
-	
+	clientsocket.send(msg)
